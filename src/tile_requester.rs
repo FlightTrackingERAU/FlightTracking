bda47971b18b2a8656bcf51c3d7c342251703bf6
Troy Neubauer <troyneubauer@gmail.com>
2021-10-15 23:08:29 -0400

    Switched to async channels and away from thread::spawn

--- src/tile_requester.rs
+++ src/tile_requester.rs
@@ -1,16 +1,13 @@
-use std::sync::mpsc::{Receiver, SyncSender};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 
 use maptiler_cloud::{ConstructedRequest, Maptiler, TileRequest};
 use tokio::runtime::Runtime;
 
 use crate::tile_cache::{Tile, TileId};
 
-const TILE_BUFFER_SIZE: usize = 1024;
-const REQUEST_BUFFER_SIZE: usize = 1024;
-
 pub struct TileRequester {
-    tile_rx: tokio::sync::mpsc::Receiver<Tile>,
-    request_tx: SyncSender<TileId>,
+    tile_rx: UnboundedReceiver<Tile>,
+    request_tx: UnboundedSender<TileId>,
 }
 
 impl TileRequester {
@@ -19,9 +16,9 @@ impl TileRequester {
         S: AsRef<str>,
     {
         // The channel with which the thread can send back the tiles that we requested
-        let (tile_tx, tile_rx) = tokio::sync::mpsc::channel(TILE_BUFFER_SIZE);
+        let (tile_tx, tile_rx) = tokio::sync::mpsc::unbounded_channel();
         // This channel allows us to send and receive tile requests
-        let (request_tx, request_rx) = std::sync::mpsc::sync_channel(REQUEST_BUFFER_SIZE);
+        let (request_tx, request_rx) = tokio::sync::mpsc::unbounded_channel();
 
         let api_key = api_key.as_ref().to_string();
 
@@ -49,8 +46,8 @@ impl TileRequester {
 
 async fn request_loop(
     api_key: String,
-    tile_tx: tokio::sync::mpsc::Sender<Tile>,
-    request_rx: Receiver<TileId>,
+    tile_tx: UnboundedSender<Tile>,
+    mut request_rx: UnboundedReceiver<TileId>,
 ) {
     // Can customize the runtime parameters later
     // This uses expect(), because we are already in another thread, we would kind of already be in
@@ -58,7 +55,7 @@ async fn request_loop(
     let maptiler = Maptiler::new(api_key);
 
     loop {
-        if let Ok(tile_id) = request_rx.recv() {
+        if let Some(tile_id) = request_rx.recv().await {
             // This should panic to aid in educability
             // Create the tile request
             let tile_request = TileRequest::new(
@@ -73,27 +70,19 @@ async fn request_loop(
             let request = maptiler.create_request(tile_request);
 
             //TODO: Maybe look at priorities here and limit pending requests to a reasonable number when under load
-            // Spawn the request function to be awaited
-            tokio::spawn(request_tile(
-                tokio::sync::mpsc::Sender::clone(&tile_tx),
-                tile_id,
-                request,
-            ));
+
+            // Spawn the request function. Will push the tile to tile_tx when the request completes
+            let tile_tx = tile_tx.clone();
+            tokio::spawn(async move {
+                if let Ok(tile_bytes) = request.execute().await {
+                    // Create an RGBA image from the JPEG bytes
+                    let image = image::load_from_memory(&tile_bytes).unwrap().into_rgba();
+
+                    let tile = Tile { id: tile_id, image };
+
+                    tile_tx.send(tile).ok();
+                }
+            });
         }
-    }
-}
-
-async fn request_tile(
-    tile_tx: tokio::sync::mpsc::Sender<Tile>,
-    id: TileId,
-    request: ConstructedRequest,
-) {
-    if let Ok(tile_bytes) = request.execute().await {
-        // Create an RGBA image from the JPEG bytes
-        let image = image::load_from_memory(&tile_bytes).unwrap().into_rgba();
-
-        let tile = Tile { id, image };
-
-        tile_tx.send(tile).await.ok();
     }
 }
