84a0742333f79c177fcb6f0c7f486936cd16711b
newcomb-luke <newcomb.luke@gmail.com>
2021-11-01 15:59:19 -0400

    Airports are loaded from a binary file

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use conrod_core::{
 use glam::DVec2;
 use glium::Surface;
 
+mod airports;
 mod button_widget;
 mod map;
 mod map_renderer;
@@ -13,6 +14,7 @@ mod tile_requester;
 mod ui_filter;
 mod util;
 
+pub use airports::*;
 pub use button_widget::*;
 pub use map::*;
 pub use map_renderer::*;
@@ -26,7 +28,7 @@ const HEIGHT: u32 = 720;
 
 const MAX_ZOOM_LEVEL: u32 = 20;
 
-widget_ids!(pub struct Ids { debug_menu[], text, viewport, map_images[], squares[], tiles[], square_text[], weather_button, airplane_button, latitude_lines[], latitude_text[], longitude_lines[], longitude_text[], filter_widget });
+widget_ids!(pub struct Ids { debug_menu[], text, viewport, map_images[], squares[], tiles[], square_text[], weather_button, airplane_button, latitude_lines[], latitude_text[], longitude_lines[], longitude_text[], filter_widget, airports[] });
 
 pub use util::PERF_DATA;
 
@@ -56,7 +58,7 @@ pub fn run_app() {
 
     let noto_sans_ttf = include_bytes!("../assets/fonts/NotoSans/NotoSans-Regular.ttf");
     let noto_sans = Font::from_bytes(noto_sans_ttf).expect("Failed to decode font");
-    let noto_sans = ui.fonts.insert(noto_sans);
+    let _noto_sans = ui.fonts.insert(noto_sans);
 
     let b612_ttf = include_bytes!("../assets/fonts/B612Mono/B612Mono-Regular.ttf");
     let b612 = Font::from_bytes(b612_ttf).expect("Failed to decode font");
@@ -70,6 +72,9 @@ pub fn run_app() {
     let runtime = tokio::runtime::Runtime::new().expect("Unable to create Tokio runtime!");
 
     let mut tile_cache = TileCache::new(&runtime);
+
+    let airports_bin = include_bytes!("../assets/data/airports.bin");
+    let airports = airports_from_bytes(airports_bin).expect("Failed to load airports");
 
     let mut should_update_ui = true;
     let mut viewer = map::TileView::new(0.0, 0.0, 2.0, 1080.0 / 2.0);
@@ -145,6 +150,10 @@ pub fn run_app() {
                         &mut ids,
                         ui,
                     );
+
+                    //========== Draw Airports ==========
+
+                    airports::airport_renderer::draw(&airports, &viewer, &display, &mut ids, ui);
 
                     //========== Draw Debug Text ==========
                     let data = {
