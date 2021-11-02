84b0acced3f50122006eed2fd84976ffc0c6d4b5
newcomb-luke <newcomb.luke@gmail.com>
2021-11-02 18:09:14 -0400

    Added bounds checking for plane rendering

--- src/plane_renderer.rs
+++ src/plane_renderer.rs
@@ -17,19 +17,28 @@ pub fn draw(
         .resize(planes.len(), &mut ui.widget_id_generator());
 
     let viewport = view.get_world_viewport(ui.win_w, ui.win_h);
+    let lat_top = crate::util::latitude_from_y(viewport.top_left.y.rem_euclid(1.0)) as f32;
+    let lat_bottom = crate::util::latitude_from_y(viewport.bottom_right.y.rem_euclid(1.0)) as f32;
+    let long_left = crate::util::longitude_from_x(viewport.top_left.x.rem_euclid(1.0)) as f32;
+    let long_right = crate::util::longitude_from_x(viewport.bottom_right.x.rem_euclid(1.0)) as f32;
+
     for (i, plane) in planes.iter().enumerate() {
-        let world_x = util::x_from_longitude(plane.longitude as f64);
-        let world_y = util::y_from_latitude(plane.latitude as f64);
+        if (plane.latitude > lat_bottom && plane.latitude < lat_top)
+            && (plane.longitude > long_left && plane.longitude < long_right)
+        {
+            let world_x = util::x_from_longitude(plane.longitude as f64);
+            let world_y = util::y_from_latitude(plane.latitude as f64);
 
-        let pixel_x = world_x_to_pixel_x(world_x, &viewport, ui.win_w);
-        let pixel_y = world_y_to_pixel_y(world_y, &viewport, ui.win_h);
+            let pixel_x = world_x_to_pixel_x(world_x, &viewport, ui.win_w);
+            let pixel_y = world_y_to_pixel_y(world_y, &viewport, ui.win_h);
 
-        let rect_fil = [50.0; 2];
+            let rect_fil = [50.0; 2];
 
-        conrod_core::widget::Rectangle::fill(rect_fil)
-            .x_y(pixel_x, pixel_y)
-            .w_h(20.0, 20.0)
-            .color(conrod_core::color::BLACK)
-            .set(ids.planes[i], ui)
+            conrod_core::widget::Rectangle::fill(rect_fil)
+                .x_y(pixel_x, pixel_y)
+                .w_h(10.0, 10.0)
+                .color(conrod_core::color::BLACK)
+                .set(ids.planes[i], ui);
+        }
     }
 }
