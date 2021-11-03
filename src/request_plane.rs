8a6f37e0df24c2466c97f31d9408fde4d5bb3da2
HernanW4 <zhernandezwalter@gmail.com>
2021-11-03 14:39:46 -0400

    Better documentation on how rendering the planes work. Some optimization made for FPS.

--- src/request_plane.rs
+++ src/request_plane.rs
@@ -3,12 +3,18 @@ use tokio::{runtime::Runtime, time::Instant};
 
 use opensky_api::errors::Error;
 
+///The body of a Plane
+///
+///Right Now we only care about Long and Lat;
+///It will maybe be bigger depending on things we may like
+///The planes to do.
 #[derive(Debug)]
 pub struct Plane {
     pub longitude: f32,
     pub latitude: f32,
 }
 impl Plane {
+    ///Constructor on to make a new Plane
     pub fn new(longitude: f32, latitude: f32) -> Self {
         Plane {
             longitude,
@@ -17,11 +23,14 @@ impl Plane {
     }
 }
 
+///Structure to save te Plane data we request
+///We put it into an Arc and Mutex to make it easier to read.
 pub struct PlaneRequester {
     planes_storage: Arc<Mutex<Arc<Vec<Plane>>>>,
 }
 
 impl PlaneRequester {
+    ///Constructor on how to request the plane data.
     pub fn new(runtime: &Runtime) -> Self {
         let planes_storage = Arc::new(Mutex::new(Arc::new(Vec::new())));
 
@@ -30,12 +39,19 @@ impl PlaneRequester {
         PlaneRequester { planes_storage }
     }
 
+    ///Returns a clone of the Mutex list of planes.
     pub fn planes_storage(&self) -> Arc<Vec<Plane>> {
         let guard = self.planes_storage.lock().unwrap();
         guard.clone()
     }
 }
 
+///Loop to get plane data.
+///Some math had to be done for the sleeping time.
+///
+///The OpenSky Api gets data every 5-6 seconds,
+///the function must also follow that running time.
+///
 async fn plane_data_loop(list_of_planes: Arc<Mutex<Arc<Vec<Plane>>>>) {
     loop {
         let start = Instant::now();
@@ -54,20 +70,19 @@ async fn plane_data_loop(list_of_planes: Arc<Mutex<Arc<Vec<Plane>>>>) {
         let time_interval = tokio::time::Duration::from_secs(5);
         let seconds = end - start;
 
-        println!("Took: {:?}", seconds);
         let sleep_time = if seconds <= tokio::time::Duration::from_secs(5) {
             time_interval - seconds
         } else {
             tokio::time::Duration::from_secs(0)
         };
 
-        println!("Sleep time: {:?}", sleep_time);
-
         tokio::time::sleep(sleep_time).await;
     }
 }
 
-//Request the plane data and makes it into a Vec
+///In here we call the OpenSky Api to get the data from planes.
+///
+///Request the plane data and makes it into a Vec.
 async fn request_plane_data() -> Result<Vec<Plane>, Error> {
     let open_sky = opensky_api::OpenSkyApi::new();
 
